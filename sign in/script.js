9cfb0b2dc61dd7840027d1f36fd39170e00b1f00
Jake Wayland <jawjake13@gmail.com>
2026-05-28 10:16:20 -0400

    signin form edit

--- sign in/script.js
+++ sign in/script.js
@@ -5,17 +5,8 @@ submit = document.getElementById("submit")
 errormsg = document.getElementById("alert")
 
 signinform.addEventListener('submit', (e)=>{
-    if(username.value = ""){
-        e.preventDefault();
-        errormsg.innerHTML = "Please enter a username."
-    }
-
-    else if(password.value = ""){
-        e.preventDefault();
-        errormsg.innerHTML = "Please enter a username."
-    }
-
-    else if(username.value != "user123" || password.value != "cheesecake") {
+    
+    if(username.value != "user123" || password.value != "cheesecake") {
         e.preventDefault();
         errormsg.innerHTML = "Please enter the correct password."
     }
